da428dffece5e6a2ce25218435daa58107daed2b
hmamaysky <harry.mamaysky@gmail.com>
2021-06-17 10:38:02 -0400

    change formatting of series plots

---
+++ Analysis/driver_energy.py
@@ -0,0 +1,17 @@
+from energy import energy as en
+
+# %% read in text data
+txtd = en.read_info()
+
+# %% read in the processed data
+serd = en.read_processed()
+en.summary(serd)  ## summary of series
+
+# %% search for outlier event
+en.find_outliers(serd,txtd)
+
+# %% display outlier events
+en.show_outliers(serd,txtd)
+
+# %% some text series plots
+en.plot_text_stats(serd)
