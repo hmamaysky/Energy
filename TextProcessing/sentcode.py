55d92d2930500d6750c204f7c6559f0ccc3c73e6
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-05-24 12:58:01 -0400

    combine total.py and sentcode.py

--- TextProcessing/sentcode.py
+++ TextProcessing/sentcode.py
@@ -2,6 +2,7 @@
 
 """
     Function           : This code prepares the sentiment scores
+                        and counts the total number of words after cleaning for each article
 """
 
 import numpy as np
@@ -30,8 +31,10 @@ def parse_option():
     parser.add_argument('--sentDicPath', type=str, default='2014.txt')
     parser.add_argument('--inputPath', type=str, 
            default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info')
-    parser.add_argument('--outputPath', type=str, 
+    parser.add_argument('--outputPathSent', type=str, 
            default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/sentiment')
+    parser.add_argument('--outputPathTotal', type=str, 
+           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/total')
     opt = parser.parse_args()
     return opt
 
@@ -39,21 +42,59 @@ opt = parse_option()
 print(opt)
 
 
-def main():
-    
-    def get_Neg(sample):
+def get_Neg(sample):
         words = [word for word in sample if word in Neg]
         return ' '.join(words)
 
-    def get_Pos(sample):
-        words = [word for word in sample if word in Pos]
-        return ' '.join(words)
+def get_Pos(sample):
+    words = [word for word in sample if word in Pos]
+    return ' '.join(words)
 
-    # def get_uncertain(sample):
-    #     sample = sample.split(' ')
-    #     words = [word for word in sample if word in Uncertain]
-    #     return ' '.join(words)
-    
+# def get_uncertain(sample):
+#     sample = sample.split(' ')
+#     words = [word for word in sample if word in Uncertain]
+#     return ' '.join(words)
+
+
+def write_sent_total(file):
+    YYYYMM = file[-15:-9]
+    Temp = pd.read_csv(f"{opt.inputPath}/{file}",sep=',',encoding = "ISO-8859-1")
+    Temp['augbod'] = Temp['augbod'].str.lower()
+    Temp['body_stem'] = Temp['augbod'].parallel_apply(get_clean)
+    Temp['body_negation'] = Temp['augbod'].parallel_apply(mark_neg)
+    Temp['body_Neg'] = Temp['body_negation'].parallel_apply(get_Neg)
+    Temp['body_Pos'] = Temp['body_negation'].parallel_apply(get_Pos)
+    Temp['body_total'] = Temp['body_stem'].parallel_apply(get_total)
+
+    ngram_Neg = textmining.TermDocumentMatrix()
+    for f in Temp['body_Neg']:
+        ngram_Neg.add_doc(f)
+
+    ngram_Pos = textmining.TermDocumentMatrix()
+    for f in Temp['body_Pos']:
+        ngram_Pos.add_doc(f)
+
+    Final = []
+    Id = list(Temp['Id'])
+    for i in range(len(Id)):
+        temp_pos = []
+        temp_neg = []
+        pos = 0
+        neg = 0
+        for words,num in ngram_Pos.sparse[i].items():
+            pos += num
+        for words,num in ngram_Neg.sparse[i].items():
+            neg += num
+        Final.append((Id[i],(pos-neg)/Temp['body_total'][i]))
+    df_sent = pd.DataFrame(Final,columns = ['Id','sent'])   
+    df_sent.to_csv(f"{opt.outputPathSent}/{YYYYMM}_sent.csv",index=False)
+
+    df_total = Temp[['Id','body_total']].rename(columns={'body_total': 'total'})
+    df_total.to_csv(f"{opt.outputPathTotal}/{YYYYMM}_total.csv",index=False)
+
+        
+if __name__ == "__main__":
+
     with open(opt.sentDicPath, 'r') as f:
         content = f.readlines()
         Line = []
@@ -62,12 +103,12 @@ def main():
             line = re.sub('[^A-Za-z]', ' ', line)
             line_list = line.strip().split('\n')
             Line.extend(line_list)
-    
+
     neg_index = Line.index('NEGATIVE')
     pos_index = Line.index('POSITIVE')
     uncertain_index = Line.index('UNCERTAINTY')
     #LITIGIOUS_index = Line.index('LITIGIOUS')
-    
+
     neg = Line[neg_index+1:pos_index]
     pos = Line[pos_index+1:uncertain_index]
     #uncertain = Line[uncertain_index+1:LITIGIOUS_index]
@@ -75,47 +116,11 @@ def main():
     Neg = [l.lower() for l in neg]
     Pos = [l.lower() for l in pos]
     #Uncertain = [l.lower() for l in uncertain]
-    
+
     # !!crucial for efficiency!!
     Neg = set(Neg)
     Pos = set(Pos)
-        
+
     for file in tqdm(os.listdir(opt.inputPath)):
-        YYYYMM = file[-15:-9]
-        Temp = pd.read_csv(f"{opt.inputPath}/{file}",sep=',',encoding = "ISO-8859-1")
-        Temp['augbod'] = Temp['augbod'].str.lower()
-        Temp['body_stem'] = Temp['augbod'].parallel_apply(get_clean)
-        Temp['body_negation'] = Temp['augbod'].parallel_apply(mark_neg)
-        Temp['body_Neg'] = Temp['body_negation'].parallel_apply(get_Neg)
-        Temp['body_Pos'] = Temp['body_negation'].parallel_apply(get_Pos)
-        Temp['body_total'] = Temp['body_stem'].parallel_apply(get_total)
+        write_sent_total(file)
 
-        ngram_Neg = textmining.TermDocumentMatrix()
-        for f in Temp['body_Neg']:
-            ngram_Neg.add_doc(f)
-
-        ngram_Pos = textmining.TermDocumentMatrix()
-        for f in Temp['body_Pos']:
-            ngram_Pos.add_doc(f)
-
-        Final = []
-        Id = list(Temp['Id'])
-        for i in range(len(Id)):
-            temp_pos = []
-            temp_neg = []
-            pos = 0
-            neg = 0
-            for words,num in ngram_Pos.sparse[i].items():
-                pos += num
-            for words,num in ngram_Neg.sparse[i].items():
-                neg += num
-            Final.append((Id[i],(pos-neg)/Temp['body_total'][i]))
-
-
-        df = pd.DataFrame(Final,columns = ['Id','sent'])   
-        df.to_csv(f"{opt.outputPath}/{YYYYMM}_sent.csv",index=False)
-
-        
-if __name__ == "__main__":
-
-    main()
