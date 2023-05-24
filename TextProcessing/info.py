34c546556445c9e778521d748cb4a219e5b4f0d2
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-05-24 14:25:19 -0400

    Update info.py

--- TextProcessing/info.py
+++ TextProcessing/info.py
@@ -1,14 +1,32 @@
-#!/apps/anaconda2/bin/python
+#!/user/kh3191/.conda/envs/nlp/bin/python
 
 """
-    Program            : refer to run_info.sh 
     Function           : This code combine all the article measures and change the time to NY
 """
 
 import pandas as pd
-import sys
 from dateutil import tz
 from datetime import datetime
+# from pandarallel import pandarallel
+# pandarallel.initialize(progress_bar=False)
+from tqdm import tqdm
+tqdm.pandas()
+
+import argparse
+from argparse import RawTextHelpFormatter
+def parse_option():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--inputPath', type=str, 
+           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info')
+    parser.add_argument('--measurePath', type=str, 
+           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure')
+    parser.add_argument('--outputPath', type=str, 
+           default='../../../..//shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/combined_info')
+       opt = parser.parse_args()
+    return opt
+
+opt = parse_option()
+print(opt)
 
 
 #################
@@ -33,47 +51,37 @@ def UTC_to_NY(row):
 ################
 
 
+if __name__ == "__main__":
 
-
-aa = sys.argv[1]
-
-path_sent = '/NOBACKUP/scratch/ra2826/oil-project/sentiment' 
-path_topic = '/NOBACKUP/scratch/ra2826/oil-project/topic_allocation'
-path_entropy = '/NOBACKUP/scratch/ra2826/oil-project/entropy'
-path_total = '/NOBACKUP/scratch/ra2826/oil-project/total'
-path_info = '/NOBACKUP/scratch/ra2826/oil-project/oil_RTRS'
-path_out = '/NOBACKUP/scratch/ra2826/oil-project/info'                 
-
-
-df0=pd.DataFrame()
-
-
-df_sent = pd.read_csv(path_sent+ '/' + aa + 'sent.csv', delimiter=',')
-df_sent.rename(columns={'sent': 'sentiment'}, inplace=True)
-df_topic = pd.read_csv(path_topic+ '/' + aa + 'topic_alloc.csv', delimiter=',')
-df_topic.drop(['headline'], axis=1, inplace=True)
-df_entropy = pd.read_csv(path_entropy+ '/' + aa + '_entropy.csv', delimiter=',')
-df_total = pd.read_csv(path_total+ '/' + aa + 'total.csv', delimiter=',')
-df_info = pd.read_csv(path_info + '/' + 'oil_RTRS_' + aa + '.csv', delimiter=',')
-df_info.drop(['augbod'], axis=1, inplace=True)
-
-
-df_all0=df_info.join(df_sent['sentiment'])
-df_all1=df_all0.join(df_topic)
-df_all2=df_all1.join(df_entropy['entropy'])
-df_all3=df_all2.join(df_total['total'])
-df0=df_all3
+    for file in tqdm(os.listdir(opt.inputPath)):
+        YYYYMM = file[-15:-9]
         
-
-
-
-df0['TimeStamp_NY'] = df0.apply(UTC_to_NY, axis=1)
-df0.rename(columns={'TimeStamp': 'TimeStamp_UTC'}, inplace=True)
-
-cols = ['Id', 'TimeStamp_UTC', 'TimeStamp_NY', 'subject', 'headline', 'entropy', 'total', 'sentiment', 'Topic1', 'Topic2', 'Topic3', 'Topic4', 'Topic5', 'Topic6', 'Topic7']
-df0 = df0[cols]
+        df_info = pd.read_csv(f"{opt.inputPath}/{file}", delimiter=',')
+        df_info.drop(['augbod'], axis=1, inplace=True)
         
-df0.to_csv(path_out+ '/' + aa + '_info.csv',index=False)	
+        df_sent = pd.read_csv(f"{opt.measurePath}/{YYYMM}_sent.csv", delimiter=',')
+        df_sent.rename(columns={'sent': 'sentiment'}, inplace=True)
+        df_topic = pd.read_csv(f"{opt.measurePath}/{YYYMM}_topic_alloc.csv", delimiter=',')
+        df_topic.drop(['headline'], axis=1, inplace=True)
+        try:
+            df_entropy = pd.read_csv(f"{opt.measurePath}/{YYYMM}_entropy.csv", delimiter=',')
+        except FileNotFoundError:
+            df_entropy = pd.DataFrame(columns=['entropy'])
+        df_total = pd.read_csv(f"{opt.measurePath}/{YYYMM}_total.csv", delimiter=',')
+    
+        df = df_info.join(df_sent['sentiment'])\
+                    .join(df_topic)\
+                    .join(df_entropy['entropy'])\
+                    .join(df_total['total'])
+        
+        df['TimeStamp_NY'] = df.progress_apply(UTC_to_NY, axis=1)
+        df.rename(columns={'TimeStamp': 'TimeStamp_UTC'}, inplace=True)
+
+        cols = ['Id', 'TimeStamp_UTC', 'TimeStamp_NY', 'subject', 'headline', 'entropy', 'total', 'sentiment', 
+                'Topic1', 'Topic2', 'Topic3', 'Topic4', 'Topic5', 'Topic6', 'Topic7']
+        df = df[cols]
+
+        df.to_csv(f"{opt.outputPath}/{YYYYMM}_info.csv",index=False)
 
 
 
