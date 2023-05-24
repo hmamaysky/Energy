5987f959aa0defb6ffdc95072bbf44802ce0ce9e
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-05-24 15:18:21 -0400

    Update dtm_numeric.py

--- TextProcessing/dtm_numeric.py
+++ TextProcessing/dtm_numeric.py
@@ -1,56 +1,67 @@
-#!/apps/anaconda2/bin/python
+#!/user/kh3191/.conda/envs/nlp/bin/python
  
 """
-    Program            : refer to run_dtm_numeric.sh 
     Function           : This code converts the words and article ids in dtm files to numbers.
-    					The reason is: this is the format that the sparse matrix in cosine code gets	
+                        The reason is: this is the format that the sparse matrix in cosine code gets
 """
 
 import pandas as pd
-import sys
+import os
+from tqdm import tqdm
+
+import argparse
+from argparse import RawTextHelpFormatter
+def parse_option():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--inputWordsPath', type=str, 
+           default='clustering_C.csv')
+    parser.add_argument('--dtmPath', type=str, 
+           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/dtm_Clustering_C')
+    parser.add_argument('--concatInfoPath', type=str, 
+           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/concat/info_concatenate.csv')
+    parser.add_argument('--outputPath', type=str, 
+           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/dtm_numeric')
+    opt = parser.parse_args()
+    return opt
+
+opt = parse_option()
+print(opt)
 
 
-j= sys.argv[1]
-dtmpath= '/work/hw2676/Energy/dtm_Clustering_C'
-df = pd.read_csv(dtmpath + '/' + j)
-df = pd.melt(df, id_vars=list(df)[:2], value_vars=list(df)[2:], var_name='words', value_name='freq')
-df = df[df['freq']!=0]  
-
-word_column = df.words.tolist()
-id_column = df.Id.tolist()
-freq_column = df.freq.tolist()
-
-
-words_test = pd.read_csv('/user/hw2676/code/Energy/DataProcessing/article_measure/dtm/clustering_C.csv', sep=',',names=['word','Topic','freq'])
+words_test = pd.read_csv(opt.inputWordsPath, sep=',', names=['word','Topic','freq'])
 word_set = words_test['word'][1:].tolist()
-
-
-idpath = '/work/hw2676/Energy/concat'
-df_selected = pd.read_csv(idpath + '/' + 'Info_concat.csv',sep=',')
-id_set = df_selected.Id.tolist()
-
-
 dic_word = {k: v for v, k in enumerate(word_set)}
 def func_word(item):
-    return dic_word[item]  
+    return dic_word[item]
 
-
+df_selected = pd.read_csv(opt.concatInfoPath, sep=',')
+id_set = df_selected.Id.tolist()
 dic_id = {k: v for v, k in enumerate(id_set)}
 def func_id(item):
-    return dic_id[item]  
+    return dic_id[item]
 
 
-V = freq_column
-I = map(func_id,id_column)
-J = map(func_word,word_column)
+if __name__ == "__main__":
+    
+    for file in tqdm(os.listdir(opt.dtmPath)):
 
+        df = pd.read_csv(f"{opt.dtmPath}/{file}")
+        df = pd.melt(df, id_vars=list(df)[:2], value_vars=list(df)[2:], var_name='words', value_name='freq')
+        df = df[df['freq']!=0]
 
-df_out = pd.DataFrame()
-df_out['freq'] = V
-df_out['Id'] = I
-df_out['words'] = J
+        word_column = df.words.tolist()
+        id_column = df.Id.tolist()
+        freq_column = df.freq.tolist()
 
-outputpath= '/work/hw2676/Energy/dtm_numeric'
-df_out.to_csv(outputpath + '/' + j,index=False)
+        V = freq_column
+        I = list(map(func_id,id_column))
+        J = list(map(func_word,word_column))
+
+        df_out = pd.DataFrame()
+        df_out['freq'] = V
+        df_out['Id'] = I
+        df_out['words'] = J
+
+        df_out.to_csv(f"{opt.outputPath}/{file}",index=False)
 
 
