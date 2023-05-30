29b64587af2397c234a624257e9dd0e786d83ff1
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-05-30 02:41:45 -0400

    Update utils.py

--- TextProcessing/utils.py
+++ TextProcessing/utils.py
@@ -3,6 +3,9 @@ from nltk.tokenize import word_tokenize
 from nltk.sentiment.util import mark_negation
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
+from nltk import stem 
+stemmer = stem.snowball.EnglishStemmer()
+from nltk.util import ngrams
 
 ######################################################## 
 # 
@@ -21,6 +24,95 @@ def get_clean(sample):
     result = ' '.join(words)
     return result
 
+
+def get_clean0(sample):
+
+    sample = sample.upper().split('\n')
+    sample = ['' if (('NEWSDESK' in xx) & (len(sample)-yy <= 5 )) else xx for yy, xx in enumerate(sample)]
+    sample = ' '.join(sample)
+    sample = re.sub(r'\(.*?\)','',sample)
+    sample = re.sub(r'<.*?>','',sample)
+    
+    sample = re.sub('CBOT|CHICAGO BOARD OF TRADE|CHICAGO MERCANTILE EXCHANGE|CME','',sample)
+
+    sample = sample.lower()
+    sample = re.sub('s&p', 'snp', sample)
+    sample = re.sub('s & p', 'snp', sample)
+    sample = re.sub("standard[\s]&[\s]poor's", 'snp', sample)
+    sample = re.sub("standard[\s]and[\s]poor's", 'snp', sample)
+    sample = re.sub("snp[\s]500", 'snp500', sample)
+    sample = re.sub("dow[\s]jones[\s]industrial[\s]average", 'djia', sample)
+    sample = re.sub("new[\s]york[\s]stock[\s]exchange", 'nyse', sample)
+    sample = re.sub("london[\s]stock[\s]exchange", 'ftse', sample)
+    sample = re.sub("stock[\s]exchange[\s]of[\s]hong[\s]kong", 'sehk', sample)
+    sample = re.sub("australian[\s]stock[\s]exchange", 'asx', sample)
+    sample = re.sub("fannie[\s]mae", 'fnma', sample)
+    sample = re.sub("freddie[\s]mac", 'fdmc', sample)
+    sample = re.sub("federal[\s]reserve", 'fed', sample)
+    sample = re.sub("securities[\s]and[\s]exchange[\s]commission", 'sec', sample)
+    sample = re.sub("chief[\s]executive[\s]officer", 'ceo', sample)
+    sample = re.sub("chief[\s]financial[\s]officer", 'cfo', sample)
+    sample = re.sub("chief[\s]operating[\s]officer", 'coo', sample)
+    sample = re.sub("chief[\s]investment[\s]officer", 'cio', sample)
+    sample = re.sub("vice[\s]president", 'vp', sample)
+    sample = re.sub("international[\s]monetary[\s]fund", 'imf', sample)
+    sample = re.sub('[0-9]{9}[0-9]+', ' _bn_ ', sample)
+    sample = re.sub('[0-9]{6}[0-9]+', ' _mn_ ', sample)
+    sample = re.sub('_n_[\s]+billion', ' _bn_ ', sample)
+    sample = re.sub('_n_[\s]+million', ' _mn_ ', sample)
+    sample = ' '.join(s for s in sample.split() if not any(c.isdigit() for c in s))
+
+    document = re.sub('[^a-z]', ' ', sample)
+    cleanup = document.strip().split()
+    words = [word for word in cleanup if word not in set(stop)]
+    result = ' '.join(words)
+    return result
+
+
+def get_clean4(sample):
+    Trigrams = []
+    sample = sample.upper().split('\n')
+    sample = ' '.join(sample)
+    sample = re.sub(r'\(.*?\)','',sample)
+    sample = re.sub(r'<.*?>','',sample)
+    total = sample.replace('?', '***').replace('!', '***').replace('.', '***').replace(':', '***').replace(';', '***').split('***')
+
+    for l in total:
+        lx = get_clean0(l)
+        tokens = word_tokenize(lx)
+        cleanup = [stemmer.stem(token) for token in tokens]
+        trigrams = ngrams(cleanup,4)
+        Trigrams.extend(['.'.join(lx) for lx in trigrams])
+
+    return Trigrams
+
+
 def get_total(sample):
     cleanup = sample.split(' ')
     return len(cleanup)
+
+
+class NGRAM(object):
+    
+    def __init__(self):
+        # Set tokenizer to use for tokenizing new documents
+        # The term document matrix is a sparse matrix represented as a
+        # list of dictionaries. Each dictionary contains the word
+        # counts for a document.
+        self.sparse = []
+        # Keep track of the number of documents containing the word.
+        self.doc_count = {}
+
+    def add_doc(self, document):
+        """Add document to the term-document matrix."""
+        # Split document up into list of strings
+        words = document
+        # Count word frequencies in this document
+        word_counts = {}
+        for word in words:
+            word_counts[word] = word_counts.get(word, 0) + 1
+        # Add word counts as new row to sparse matrix
+        self.sparse.append(word_counts)
+        # Add to total document count for each word
+        for word in word_counts:
+            self.doc_count[word] = self.doc_count.get(word, 0) + 1
