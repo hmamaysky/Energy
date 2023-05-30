6673188391a03dd3a10f4b9845b274d696fba95f
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-05-30 03:24:54 -0400

    Update entropy.py

--- TextProcessing/entropy.py
+++ TextProcessing/entropy.py
@@ -7,7 +7,6 @@
 
 import numpy as np
 import pandas as pd
-import torch
 from pandarallel import pandarallel
 pandarallel.initialize(progress_bar=False)
 
@@ -17,6 +16,28 @@ import os
 import glob
 
 from tqdm import tqdm
+
+
+######################################################## 
+# 
+# File Paths 
+# 
+########################################################
+import argparse
+from argparse import RawTextHelpFormatter
+def parse_option():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument('--ngPath', type=str, 
+           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure')
+    parser.add_argument('--inputPath', type=str, 
+           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info')
+    parser.add_argument('--monthTrials', type=int, default=27)
+    parser.add_argument('--monthWindow', type=int, default=24)
+    opt = parser.parse_args()
+    return opt
+
+opt = parse_option()
+print(opt)
 
 
 # from collections import defaultdict
@@ -34,60 +55,59 @@ def get_stop_dict(ngr_file):
     stop_dict = Counter(dict(zip(df['word'], df['freq'])))
     return stop_dict
 
+import json
+def save_list_counters(counters, file_name):
+    counters_data = [{k: v for k, v in counter.items()} for counter in counters]
+    with open(file_name, 'w') as f:
+        json.dump(counters_data, f)
+        
+def load_list_counters(file_name):
+    with open(file_name, 'r') as f:
+        counters_data = json.load(f)
+        counters = [Counter(counter_dict) for counter_dict in counters_data]
+    return counters
 
-######################################################## 
-# 
-# File Paths 
-# 
-########################################################
-import argparse
-from argparse import RawTextHelpFormatter
-def parse_option():
-    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--ngPath', type=str, 
-           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure')
-    parser.add_argument('--inputPath', type=str, 
-           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info')
-    opt = parser.parse_args()
-    return opt
-
-opt = parse_option()
-print(opt)
+def get_rolling_counters(counters, fnum):
+    stop_dict = Counter()
+    for i in range(fnum-opt.monthTrials, fnum-opt.monthTrials+opt.monthWindow):
+        stop_dict += counters[i]
+    return stop_dict
 
 
 if __name__ == "__main__":
     
     data = glob.glob(opt.inputPath + '/*.csv')
     data.sort()
-    ngr3 = glob.glob(opt.ngPath + '/3gram/*.csv')
-    ngr3.sort()
-    ngr4 = glob.glob(opt.ngPath + '/4gram/*.csv')
-    ngr4.sort()
-    
-    stop3_dicts = [get_stop_dict(ngr_file) for ngr_file in tqdm(ngr3)]
-    torch.save(stop3_dicts, 'stop3_dicts.pt')
-    stop4_dicts = [get_stop_dict(ngr_file) for ngr_file in tqdm(ngr4)]
-    torch.save(stop4_dicts, 'stop4_dicts.pt')
+
+    stop34_dicts = []
+    for n in [3,4]:
+        file_name = f"stop{n}_dicts.json"
+        ngr = glob.glob(f"{opt.ngPath}/{n}gram/*.csv")
+        ngr.sort()
+        if not os.path.isfile(file_name):
+            stop_dicts = [get_stop_dict(ngr_file) for ngr_file in tqdm(ngr)]
+            save_list_counters(stop_dicts, file_name)
+            stop34_dicts.append(stop_dicts)
+        else:
+            print(f"Loading {file_name}")
+            stop34_dicts.append(load_list_counters(file_name))
+    stop3_dicts, stop4_dicts = stop34_dicts
     
     for fnum, file in tqdm(enumerate(data)):
 
         Temp = pd.read_csv(file,sep=',')
 
-        # fnum 27 indicates the number of months to use as the trial data to use for entropy calculations
-        if fnum >= 27:
+        # fnum monthTrials indicates the number of months to use as the trial data to use for entropy calculations
+        if fnum >= opt.monthTrials:
+                      
+            stop3_dict = get_rolling_counters(stop3_dicts, fnum)
+            stop4_dict = get_rolling_counters(stop4_dicts, fnum)
             
             Temp['gram4'] = Temp['augbod'].parallel_apply(get_clean4)
             ngram4s = NGRAM()
             for f in Temp['gram4']:
                 ngram4s.add_doc(f)
-                      
-#             import time
-#             start_time = time.time()
-            
-            stop3_dict = get_stop_dict(ngr3[fnum-27:fnum-3])
-            stop4_dict = get_stop_dict(ngr4[fnum-27:fnum-3])
-#             print("--- %s seconds ---" % (time.time() - start_time))
-
+                
             entropy = []
             for a in ngram4s.sparse:
                 a_sum = sum(a.values())
