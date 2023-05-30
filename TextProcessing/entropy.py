1217f42a5b0a8d6c99db507afd61288db156aa6f
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-05-30 02:40:45 -0400

    Update entropy.py

--- TextProcessing/entropy.py
+++ TextProcessing/entropy.py
@@ -1,27 +1,38 @@
 #!/user/kh3191/.conda/envs/nlp/bin/python
  
 """
-    Program            : refer to run_entropy.sh 
     Function           : Input: info, 3gram, and 4gram files
                          Output: entropy files   
 """
 
 import numpy as np
 import pandas as pd
+import torch
+from pandarallel import pandarallel
+pandarallel.initialize(progress_bar=False)
 
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk import stem 
-stemmer = stem.snowball.EnglishStemmer()
-stop = stopwords.words('english')
-from nltk.util import ngrams
+from utils import get_clean4, NGRAM
 
-import csv
-from os import listdir
-from os.path import isfile, join
+import os
+import glob
 
 from tqdm import tqdm
+
+
+# from collections import defaultdict
+# def get_stop_dict(ngr_files):
+#     stop_dict = defaultdict(int)
+#     dfs = [pd.read_csv(l, index_col=0) for l in ngr_files]
+#     df = pd.concat(dfs)
+#     for word, freq in zip(df['word'], df['freq']):
+#         stop_dict[word] += freq
+#     return stop_dict
+
+from collections import Counter
+def get_stop_dict(ngr_file):
+    df = pd.read_csv(ngr_file, index_col=0)
+    stop_dict = Counter(dict(zip(df['word'], df['freq'])))
+    return stop_dict
 
 
 ######################################################## 
@@ -33,14 +44,10 @@ import argparse
 from argparse import RawTextHelpFormatter
 def parse_option():
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--ng3Path', type=str, 
-           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/3gram')
-    parser.add_argument('--ng4Path', type=str, 
-           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/4gram')
+    parser.add_argument('--ngPath', type=str, 
+           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure')
     parser.add_argument('--inputPath', type=str, 
-           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info')
-    parser.add_argument('--outputPath', type=str, 
-           default='../../../../shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/entropy')
+           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info')
     opt = parser.parse_args()
     return opt
 
@@ -48,153 +55,52 @@ opt = parse_option()
 print(opt)
 
 
-
-SS='/'
-datas = [opt.inputPath + SS + f for f in listdir(opt.inputPath) if isfile(join(opt.inputPath, f))]
-datas.sort()
-datas2=listdir(opt.inputPath)
-datas2.sort()
-ngr3 = [opt.ng3Path + SS + f for f in listdir(opt.ng3Path) if isfile(join(opt.ng3Path, f))]
-ngr4 = [opt.ng4Path + SS + f for f in listdir(opt.ng4Path) if isfile(join(opt.ng4Path, f))]
-ngr3.sort()
-ngr4.sort()
-
-
-def get_clean0(sample):
-
-    sample = sample.upper().split('\n')
-    sample = ['' if (('NEWSDESK' in xx) & (len(sample)-yy <= 5 )) else xx for yy, xx in enumerate(sample)]
-    sample = ' '.join(sample)
-    sample = re.sub(r'\(.*?\)','',sample)
-    sample = re.sub(r'<.*?>','',sample)
-    
-    sample = re.sub('CBOT|CHICAGO BOARD OF TRADE|CHICAGO MERCANTILE EXCHANGE|CME','',sample)
-
-    sample = sample.lower()
-    sample = re.sub('s&p', 'snp', sample)
-    sample = re.sub('s & p', 'snp', sample)
-    sample = re.sub("standard[\s]&[\s]poor's", 'snp', sample)
-    sample = re.sub("standard[\s]and[\s]poor's", 'snp', sample)
-    sample = re.sub("snp[\s]500", 'snp500', sample)
-    sample = re.sub("dow[\s]jones[\s]industrial[\s]average", 'djia', sample)
-    sample = re.sub("new[\s]york[\s]stock[\s]exchange", 'nyse', sample)
-    sample = re.sub("london[\s]stock[\s]exchange", 'ftse', sample)
-    sample = re.sub("stock[\s]exchange[\s]of[\s]hong[\s]kong", 'sehk', sample)
-    sample = re.sub("australian[\s]stock[\s]exchange", 'asx', sample)
-    sample = re.sub("fannie[\s]mae", 'fnma', sample)
-    sample = re.sub("freddie[\s]mac", 'fdmc', sample)
-    sample = re.sub("federal[\s]reserve", 'fed', sample)
-    sample = re.sub("securities[\s]and[\s]exchange[\s]commission", 'sec', sample)
-    sample = re.sub("chief[\s]executive[\s]officer", 'ceo', sample)
-    sample = re.sub("chief[\s]financial[\s]officer", 'cfo', sample)
-    sample = re.sub("chief[\s]operating[\s]officer", 'coo', sample)
-    sample = re.sub("chief[\s]investment[\s]officer", 'cio', sample)
-    sample = re.sub("vice[\s]president", 'vp', sample)
-    sample = re.sub("international[\s]monetary[\s]fund", 'imf', sample)
-    sample = re.sub('[0-9]{9}[0-9]+', ' _bn_ ', sample)
-    sample = re.sub('[0-9]{6}[0-9]+', ' _mn_ ', sample)
-    sample = re.sub('_n_[\s]+billion', ' _bn_ ', sample)
-    sample = re.sub('_n_[\s]+million', ' _mn_ ', sample)
-    sample = ' '.join(s for s in sample.split() if not any(c.isdigit() for c in s))
-
-    document = re.sub('[^a-z]', ' ', sample)
-    cleanup = document.strip().split()
-    words = [word for word in cleanup if word not in set(stop)]
-    result = ' '.join(words)
-    return result
-
-
-def get_clean4(sample):
-    Trigrams = []
-    sample = sample.upper().split('\n')
-    sample = ' '.join(sample)
-    sample = re.sub(r'\(.*?\)','',sample)
-    sample = re.sub(r'<.*?>','',sample)
-    total = sample.replace('?', '***').replace('!', '***').replace('.', '***').replace(':', '***').replace(';', '***').split('***')
-
-    for l in total:
-        lx = get_clean0(l)
-        tokens = nltk.word_tokenize(lx)
-        cleanup = [stemmer.stem(token) for token in tokens]
-        trigrams = ngrams(cleanup,4)
-        Trigrams.extend(['.'.join(lx) for lx in trigrams])
-
-    return Trigrams 
-
-
-
-class NGRAM(object):
-    
-    def __init__(self):
-        # Set tokenizer to use for tokenizing new documents
-        # The term document matrix is a sparse matrix represented as a
-        # list of dictionaries. Each dictionary contains the word
-        # counts for a document.
-        self.sparse = []
-        # Keep track of the number of documents containing the word.
-        self.doc_count = {}
-
-    def add_doc(self, document):
-        """Add document to the term-document matrix."""
-        # Split document up into list of strings
-        words = document
-        # Count word frequencies in this document
-        word_counts = {}
-        for word in words:
-            word_counts[word] = word_counts.get(word, 0) + 1
-        # Add word counts as new row to sparse matrix
-        self.sparse.append(word_counts)
-        # Add to total document count for each word
-        for word in word_counts:
-            self.doc_count[word] = self.doc_count.get(word, 0) + 1
-
-
-
 if __name__ == "__main__":
     
-    for fnum, file in tqdm(enumerate(datas)):
+    data = glob.glob(opt.inputPath + '/*.csv')
+    data.sort()
+    ngr3 = glob.glob(opt.ngPath + '/3gram/*.csv')
+    ngr3.sort()
+    ngr4 = glob.glob(opt.ngPath + '/4gram/*.csv')
+    ngr4.sort()
+    
+    stop3_dicts = [get_stop_dict(ngr_file) for ngr_file in tqdm(ngr3)]
+    torch.save(stop3_dicts, 'stop3_dicts.pt')
+    stop4_dicts = [get_stop_dict(ngr_file) for ngr_file in tqdm(ngr4)]
+    torch.save(stop4_dicts, 'stop4_dicts.pt')
+    
+    for fnum, file in tqdm(enumerate(data)):
 
-        Temp=pd.read_csv(file,sep=',')
-        Temp['gram4'] = Temp['augbod'].apply(get_clean4)
-        ngram4s = NGRAM()
-        for f in Temp['gram4']:
-            ngram4s.add_doc(f)
+        Temp = pd.read_csv(file,sep=',')
 
         # fnum 27 indicates the number of months to use as the trial data to use for entropy calculations
         if fnum >= 27:
-            stop4_dict = {}
-            for l in ngr4[fnum-27:fnum-3]:
-                with open(l, 'rb') as csvfile:
-                    spamreader = csv.reader(csvfile, delimiter=',')
-                    for i,j,k in spamreader:
-                        if i!='':
-                            stop4_dict[j] = stop4_dict.get(j,0) + int(k)
-
-
-            stop3_dict = {}
-            for l in ngr3[fnum-27:fnum-3]:
-                with open(l, 'rb') as csvfile:
-                    spamreader = csv.reader(csvfile, delimiter=',')
-                    for i,j,k in spamreader:
-                        if i!='':
-                            stop3_dict[j] = stop3_dict.get(j,0) + int(k)
+            
+            Temp['gram4'] = Temp['augbod'].parallel_apply(get_clean4)
+            ngram4s = NGRAM()
+            for f in Temp['gram4']:
+                ngram4s.add_doc(f)
+                      
+#             import time
+#             start_time = time.time()
+            
+            stop3_dict = get_stop_dict(ngr3[fnum-27:fnum-3])
+            stop4_dict = get_stop_dict(ngr4[fnum-27:fnum-3])
+#             print("--- %s seconds ---" % (time.time() - start_time))
 
             entropy = []
             for a in ngram4s.sparse:
-                p = [float(l)/sum(a.values()) for l in a.values()]
-                M = []
-                for l in a.keys():
-                    b = '.'.join(l.split('.')[:3])
-                    temp = float((stop4_dict.get(l,0) + 1))/(stop3_dict.get(b,0) + 10)
-                    M.append(temp)
-                m = [-p[i] * np.log(M[i]) for i in range(len(M))]
+                a_sum = sum(a.values())
+                p = [l/a_sum for l in a.values()]
+                M = [(stop4_dict.get(l,0) + 1)/(stop3_dict.get('.'.join(l.split('.')[:3]),0) + 10) for l in a.keys()]
+                m = [-pi * np.log(Mi) for pi, Mi in zip(p,M)]
                 entropy.append(sum(m))
+                
         else:
             entropy = [np.nan] * len(Temp)
 
         entpd = pd.DataFrame({'entropy':entropy})
-        temp2 = Temp[['Id']]
-        entpd = pd.concat([entpd,temp2],axis=1)
-        jj=datas2[fnum]
-        YYYYMM=jj[-15:-9]
-        entpd.to_csv(opt.outputPath+SS+YYYYMM+'_entropy.csv',index=False)
+        entpd = pd.concat([entpd,Temp[['Id']]],axis=1)
+        
+        YYYYMM = data[fnum][-15:-9]
+        entpd.to_csv(f"{opt.ngPath}/entropy/{YYYYMM}_entropy.csv",index=False)
