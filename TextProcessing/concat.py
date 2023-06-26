1921676e679a393ef520427ac9cac9320968a711
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-06-26 14:03:58 -0400

    Update concat.py

--- TextProcessing/concat.py
+++ TextProcessing/concat.py
@@ -1,6 +1,5 @@
 #!/user/kh3191/.conda/envs/nlp/bin/python
 
-import numpy as np
 import pandas as pd
 from scipy import sparse
 
@@ -24,9 +23,7 @@ csv_files.sort()
 
 concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(csv_files)], ignore_index=True)
 output_file = '/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/concat/dtm_concatenate.npz'
-#concatenated_df.iloc[:,2:].to_csv(output_file, index=False)
 print('Converting to sparse matrix')
 sparse_matrix = sparse.csr_matrix(concatenated_df.iloc[:,2:])
 print('Writing concatenated df')
-np.savez(output_file, data=sparse_matrix.data, indices=sparse_matrix.indices,
-         indptr=sparse_matrix.indptr, shape=sparse_matrix.shape)
+sparse.save_npz(output_file, sparse_matrix)
