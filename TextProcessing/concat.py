f7d3b50a5ee33af4d0af5ed3ff102ae3076f8c38
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-06-26 13:44:04 -0400

    Update concat.py
    save dtm as sparse matrix

--- TextProcessing/concat.py
+++ TextProcessing/concat.py
@@ -1,12 +1,15 @@
 #!/user/kh3191/.conda/envs/nlp/bin/python
 
-import glob
+import numpy as np
 import pandas as pd
+from scipy import sparse
+
+import glob
 from tqdm import tqdm
 
 # concat all info
 folder_path = '/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/combined_info'
-csv_files = glob.glob(folder_path + '/*.csv')
+csv_files = glob.glob(folder_path + '/*_info.csv')
 csv_files.sort()
 
 concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(csv_files)], ignore_index=True)
@@ -16,10 +19,14 @@ concatenated_df.to_csv(output_file, index=False)
 
 # concat dtm
 folder_path = '/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/dtm_Clustering_C'
-csv_files = glob.glob(folder_path + '/*.csv')
+csv_files = glob.glob(folder_path + '/*_dtm.csv')
 csv_files.sort()
 
 concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(csv_files)], ignore_index=True)
-output_file = '/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/dtm_Clustering_C/dtm_concatenate.csv'
+output_file = '/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/concat/dtm_concatenate.npz'
+#concatenated_df.iloc[:,2:].to_csv(output_file, index=False)
+print('Converting to sparse matrix')
+sparse_matrix = sparse.csr_matrix(concatenated_df.iloc[:,2:])
 print('Writing concatenated df')
-concatenated_df.iloc[:,2:].to_csv(output_file, index=False)
+np.savez(output_file, data=sparse_matrix.data, indices=sparse_matrix.indices,
+         indptr=sparse_matrix.indptr, shape=sparse_matrix.shape)
