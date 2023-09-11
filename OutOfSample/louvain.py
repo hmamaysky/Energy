b23f30946eae5590e703636a9ee932ba2258659a
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-09-11 15:40:19 -0400

    Create louvain.py

---
+++ OutOfSample/louvain.py
@@ -0,0 +1,38 @@
+import numpy as np
+import random
+
+def get_clusters(seed, graph):
+    random.seed(seed)
+    return graph.community_multilevel(weights='cosine')
+
+def get_topic_vec(cluster, weight=True):
+    mask = np.zeros(num_words, dtype=bool)
+    mask[cluster] = True
+    if not weight:
+        return mask
+    else:
+        topic_vec = np.zeros(num_words)
+        topic_vec[mask] = [word2weight[word] for word in m.columns[cluster]]
+        return topic_vec
+    
+def get_matching_score(matrix, num_pairs=None, return_idx=False):
+    
+    """
+    return the mean of greedily-searched best matching pairs between old and new topic vectors 
+    based on max cos-similarity
+    """
+    
+    if not num_pairs:
+        num_pairs = min(matrix.shape)
+        
+    max_values, max_value_indices = [], []
+    for _ in range(num_pairs):
+        max_value_idx = np.unravel_index(np.argmax(matrix), matrix.shape)
+        max_value = matrix[max_value_idx]
+        max_values.append(max_value)
+        matrix[max_value_idx[0], :] = -np.inf
+        matrix[:, max_value_idx[1]] = -np.inf
+        if return_idx:
+            max_value_indices.append(max_value_idx)
+
+    return max_values, max_value_indices
