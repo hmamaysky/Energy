a43345d4cf13db25cac619972adf33fa9c462c36
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-09-22 16:22:25 -0400

    Update louvain.py
    additional assertion of word order in each topic (as there are multiple word list created, e.g., 387-word, 441-word, newly-updated clustering csv files)

--- OutOfSample/louvain.py
+++ OutOfSample/louvain.py
@@ -5,25 +5,29 @@ def get_clusters(seed, graph):
     random.seed(seed)
     return graph.community_multilevel(weights='cosine')
 
-def get_topic_vec(cluster, weight=True):
+def get_topic_vec(cluster, word_array, word2weight, weight=True):
+    assert word_array[0] == 'fuel' # original order of words
+    num_words = len(word_array)
     mask = np.zeros(num_words, dtype=bool)
     mask[cluster] = True
     if not weight:
         return mask
     else:
         topic_vec = np.zeros(num_words)
-        topic_vec[mask] = [word2weight[word] for word in m.columns[cluster]]
+        topic_vec[mask] = [word2weight[word] for word in word_array[cluster]]
         return topic_vec
     
-def get_matching_score(matrix, num_pairs=None, return_idx=False):
+def get_matching_score(matrix, num_pairs='min', return_idx=False):
     
     """
     return the mean of greedily-searched best matching pairs between old and new topic vectors 
     based on max cos-similarity
     """
     
-    if not num_pairs:
+    if num_pairs == 'min':
         num_pairs = min(matrix.shape)
+    elif num_pairs == 'max':
+        num_pairs = max(matrix.shape)
         
     max_values, max_value_indices = [], []
     for _ in range(num_pairs):
