3dfbb2edef122f8d9920852a153c6cb97d4ce88a
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-09-22 15:00:02 -0400

    Update run_Rolling_Topic_Models.sh
    add a test line to see output/error files

--- OutOfSample/run_Rolling_Topic_Models.sh
+++ OutOfSample/run_Rolling_Topic_Models.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chmod 700 Rolling_Topic_Models.py
+# sge_run --grid_mem=10G --grid_ncpus=1 --grid_submit=batch "./Rolling_Topic_Models.py --rolling_index=0 --GA_record_stats=True"
 for rolling_index in $(seq 0 266); do
-  sge_run --grid_mem=32G --grid_ncpus=1 --grid_submit=batch --grid_quiet "./Rolling_Topic_Models.py --rolling_index=${rolling_index}"
+  sge_run --grid_mem=10G --grid_ncpus=1 --grid_submit=batch --grid_quiet "./Rolling_Topic_Models.py --rolling_index=${rolling_index}"
 done
