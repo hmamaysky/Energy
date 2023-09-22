419e8db9be0951ac719b40862dd3c2588c5dd3cf
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-09-22 15:49:12 -0400

    Update concat.py
    save word freq on monthly basis; improve variable names

--- TextProcessing/concat.py
+++ TextProcessing/concat.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from scipy import sparse
 
-import glob
+from glob import glob
 from tqdm import tqdm
 
 
@@ -17,6 +17,8 @@ def parse_option():
            default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/dtm_Clustering_C')
     parser.add_argument('--outputPath', type=str, 
            default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/concat')
+    parser.add_argument('--freqPath', type=str, 
+           default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_freq')
     opt = parser.parse_args()
     return opt
 
@@ -24,21 +26,27 @@ opt = parse_option()
 print(opt)
 
 # concat all info
-csv_files = glob.glob(opt.combinedInfoPath + '/*_info.csv')
-csv_files.sort()
+info_files = glob(opt.combinedInfoPath + '/*_info.csv')
+info_files.sort()
 
-concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(csv_files)], ignore_index=True)
-
+concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(info_files)], ignore_index=True)
 output_file = f'{opt.outputPath}/info_concatenate.csv'
 concatenated_df.to_csv(output_file, index=False)
 
-# concat dtm
-csv_files = glob.glob(opt.dtmPath + '/*_dtm.csv')
-csv_files.sort()
 
-concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(csv_files)], ignore_index=True)
+# concat dtm
+dtm_files = glob(opt.dtmPath + '/*_dtm.csv')
+dtm_files.sort()
+
+concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(dtm_files)], ignore_index=True)
 output_file = f'{opt.outputPath}/dtm_concatenate.npz'
 print('Converting to sparse matrix')
 sparse_matrix = sparse.csr_matrix(concatenated_df.iloc[:,2:])
 print('Writing concatenated df')
 sparse.save_npz(output_file, sparse_matrix)
+
+# save word freq at monthly level
+for file in tqdm(dtm_files):
+    YYYYMM = file[-14:-8]
+    freq_series = pd.read_csv(file).iloc[:,2:].sum()
+    freq_series.to_csv(f'{opt.freqPath}/{YYYYMM}_freq.csv')
