86a49779fee0e1a443a58bbe243393eec6ce4e3d
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-09-22 17:57:49 -0400

    Update topic_allocation.py
    support local topic models

--- TextProcessing/topic_allocation.py
+++ TextProcessing/topic_allocation.py
@@ -14,14 +14,18 @@ from argparse import RawTextHelpFormatter
 def parse_option():
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument('--inputWordsPath', type=str, 
-       default='clustering_C.csv')
+       default='clustering_C.csv',
+       help='Clustering csvs are used to determine which words are contained in each topic.')
     parser.add_argument('--inputPath_info', type=str, 
-       default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info')
+       default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/oil_info',
+       help='Info files are used to retrieve headlines.')
     parser.add_argument('--inputPath_dtm', type=str, 
-       default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/dtm_Clustering_C')
+       default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/dtm_Clustering_C',
+       help='Dtms are used to sum up total word frequencies in each topic.')
     parser.add_argument('--outputPath', type=str, 
        default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/topic_allocation')
-    parser.add_argument('--n_topics', type=int, default=7)
+    parser.add_argument('--local_topic_model', type=bool, 
+       default=False)
     opt = parser.parse_args()
     return opt
 
@@ -29,22 +33,40 @@ opt = parse_option()
 print(opt)
 
 
-for file in tqdm(os.listdir(opt.inputPath_dtm)):
-    YYYYMM = file[-15:-8]
+if opt.local_topic_model:
+    assert not opt.inputWordsPath.endswith('csv')
+    inputWordsFiles = os.listdir(opt.inputWordsPath)
+    YYYYMM_list = [file[-10:-4] for file in inputWordsFiles]
+    YYYYMM_start_list = [file[-17:-11] for file in inputWordsFiles]
+else:
+    assert opt.inputWordsPath.endswith('csv')
+    YYYYMM_list = [file[-15:-8] for file in os.listdir(opt.inputPath_dtm)]
+    
 
-    df_dtm = pd.read_csv(f'{opt.inputPath_dtm}/{YYYYMM}_dtm.csv', delimiter=',')
-    df_info = pd.read_csv(f'{opt.inputPath_info}/oil_{YYYYMM}_info.csv', delimiter=',')
-    df_topics = pd.read_csv(opt.inputWordsPath, sep=',', index_col=0)
-    topics_list = [df_topics.index[df_topics['Topic'] == i].tolist() for i in range(1,opt.n_topics+1)]
+for k, YYYYMM_end in enumerate(tqdm(YYYYMM_list)):
+    
+    df_dtm = pd.read_csv(f'{opt.inputPath_dtm}/{YYYYMM_end}_dtm.csv', delimiter=',')
+    df_info = pd.read_csv(f'{opt.inputPath_info}/oil_{YYYYMM_end}_info.csv', delimiter=',')
+    
+    if opt.local_topic_model:
+        YYYYMM_start = YYYYMM_start_list[k]
+        df_topics = pd.read_csv(f'{opt.inputWordsPath}/clustering_C_{YYYYMM_start}_{YYYYMM_end}.csv', sep=',', index_col=0)
+    else:
+        df_topics = pd.read_csv(opt.inputWordsPath, sep=',', index_col=0)
+        
+    n_topics = df_topics['Topic'].max()
+    topics_list = [df_topics.index[df_topics['Topic'] == i].tolist() for i in range(1,n_topics+1)]
 
     df0 = pd.DataFrame()
     for i, topics in enumerate(topics_list):
         df0['Topic'+str(i+1)] = df_dtm[topics].sum(axis=1)
 
     df0['sum'] = df0.sum(axis=1)
-    df0 = df0.loc[:,'Topic1':f'Topic{opt.n_topics}'].div(df0['sum'], axis=0)
+    df0 = df0.loc[:,'Topic1':f'Topic{n_topics}'].div(df0['sum'], axis=0)
     df0 = df0.fillna(0)
 
-    df0['headline']=df_info['headline']
-    df0.to_csv(f'{opt.outputPath}/{YYYYMM}_topic_alloc.csv',index=False)
-
+    df0['headline'] = df_info['headline']
+    if opt.local_topic_model:
+        df0.to_csv(f'{opt.outputPath}/{YYYYMM_start}_{YYYYMM_end}_topic_alloc.csv', index=False)
+    else:
+        df0.to_csv(f'{opt.outputPath}/{YYYYMM_end}_topic_alloc.csv', index=False)
