70a12585ba5b658f6e60a4bc5ad7dd37c35e52cf
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-10-11 14:32:50 -0400

    Update topic_allocation.py

--- TextProcessing/topic_allocation.py
+++ TextProcessing/topic_allocation.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import os
 from tqdm import tqdm
+from utils import generate_month_list
 
 import argparse
 from argparse import RawTextHelpFormatter
@@ -29,25 +30,6 @@ def parse_option():
     parser.add_argument('--rolling_index', type=int, default=0)
     opt = parser.parse_args()
     return opt
-
-
-def generate_month_list(start_month, end_month):
-    start_year = int(str(start_month)[:4])
-    start_month_num = int(str(start_month)[4:])
-    
-    end_year = int(str(end_month)[:4])
-    end_month_num = int(str(end_month)[4:])
-    
-    month_list = []
-    
-    for year in range(start_year, end_year + 1):
-        start = start_month_num if year == start_year else 1
-        end = end_month_num + 1 if year == end_year else 13
-        
-        for month in range(start, end):
-            month_list.append(f"{year}{month:02}")
-    
-    return month_list
 
 
 if __name__ == '__main__':
