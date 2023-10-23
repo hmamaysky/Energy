7467d391164ffefc1cd8933ffb481ff95842f8ad
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-10-23 18:43:47 -0400

    Add files via upload
    add options to only save word freq

--- TextProcessing/concat.py
+++ TextProcessing/concat.py
@@ -5,7 +5,7 @@ from scipy import sparse
 
 from glob import glob
 from tqdm import tqdm
-
+# ./concat.py --concat_info='' --concat_dtm='' --dtmPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/NYtime_dtm_Clustering_C --save_monthly_freq=True
 
 import argparse
 from argparse import RawTextHelpFormatter
@@ -13,6 +13,8 @@ def parse_option():
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
     parser.add_argument('--combinedInfoPath', type=str, 
            default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/combined_info')
+    parser.add_argument('--concat_info', type=bool, 
+           default=True)
     parser.add_argument('--concat_dtm', type=bool, 
            default=True)
     parser.add_argument('--dtmPath', type=str, 
@@ -20,7 +22,7 @@ def parse_option():
     parser.add_argument('--outputPath', type=str, 
            default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/concat')
     parser.add_argument('--save_monthly_freq', type=bool, 
-           default=True)
+           default=False)
     parser.add_argument('--freqPath', type=str, 
            default='/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_freq')
     opt = parser.parse_args()
@@ -29,20 +31,21 @@ def parse_option():
 opt = parse_option()
 print(opt)
 
-# concat all info
-info_files = glob(opt.combinedInfoPath + '/*_info.csv')
-info_files.sort()
+if opt.concat_info:
+    # concat all info
+    info_files = glob(opt.combinedInfoPath + '/*_info.csv')
+    info_files.sort()
 
-concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(info_files)], ignore_index=True)
-output_file = f'{opt.outputPath}/info_concatenate.csv'
-concatenated_df.to_csv(output_file, index=False)
+    concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(info_files)], ignore_index=True)
+    output_file = f'{opt.outputPath}/info_concatenate.csv'
+    concatenated_df.to_csv(output_file, index=False)
 
 
 if opt.concat_dtm:
     # concat dtm
     dtm_files = glob(opt.dtmPath + '/*_dtm.csv')
     dtm_files.sort()
-
+    
     concatenated_df = pd.concat([pd.read_csv(file) for file in tqdm(dtm_files)], ignore_index=True)
     output_file = f'{opt.outputPath}/dtm_concatenate.npz'
     print('Converting to sparse matrix')
@@ -50,9 +53,13 @@ if opt.concat_dtm:
     print('Writing concatenated df')
     sparse.save_npz(output_file, sparse_matrix)
 
-    if opt.save_monthly_freq:
-        # save word freq at monthly level
-        for file in tqdm(dtm_files):
-            YYYYMM = file[-14:-8]
-            freq_series = pd.read_csv(file).iloc[:,2:].sum()
-            freq_series.to_csv(f'{opt.freqPath}/{YYYYMM}_freq.csv')
+    
+if opt.save_monthly_freq:
+    # save word freq at monthly level
+    dtm_files = glob(opt.dtmPath + '/*_dtm.csv')
+    dtm_files.sort()
+    
+    for file in tqdm(dtm_files):
+        YYYYMM = file[-14:-8]
+        freq_series = pd.read_csv(file).iloc[:,2:].sum()
+        freq_series.to_csv(f'{opt.freqPath}/{YYYYMM}_freq.csv')
