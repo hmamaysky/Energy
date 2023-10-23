575907d8a454d21bed97b393931647185e8f8700
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-10-23 19:09:00 -0400

    Update run_topic_allocation.sh
    use NYtime dtm

--- OutOfSample/run_topic_allocation.sh
+++ OutOfSample/run_topic_allocation.sh
@@ -4,5 +4,5 @@ chmod 700 topic_allocation.py
 # 5G is not enough
 # sge_run --grid_mem=20G --grid_ncpus=1 --grid_submit=batch "./topic_allocation.py --inputWordsPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_clustering_C --outputPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_topic_allocation --local_topic_model=True --rolling_index=266"
 for rolling_index in $(seq 0 266); do
-  sge_run --grid_mem=20G --grid_ncpus=1 --grid_submit=batch "./topic_allocation.py --inputWordsPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_clustering_C --outputPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_topic_allocation --local_topic_model=True --rolling_index=${rolling_index}"
+  sge_run --grid_mem=20G --grid_ncpus=1 --grid_submit=batch "./topic_allocation.py --inputWordsPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_clustering_C --inputPath_dtm=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/NYtime_dtm_Clustering_C --outputPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/article_measure/rolling_topic_allocation --local_topic_model=True --rolling_index=${rolling_index}"
 done
