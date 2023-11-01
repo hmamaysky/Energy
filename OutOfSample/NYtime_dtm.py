1d3d0c7e2294d2bab7b0835e36efb4339bc2b9e1
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-10-31 23:48:08 -0400

    Update NYtime_dtm.py
    fix weekend

--- OutOfSample/NYtime_dtm.py
+++ OutOfSample/NYtime_dtm.py
@@ -1,12 +1,13 @@
 #!/user/kh3191/.conda/envs/nlp/bin/python
 import pandas as pd
 from pandarallel import pandarallel
-pandarallel.initialize(progress_bar=False)
+pandarallel.initialize(progress_bar=True)
 
 from glob import glob
 from tqdm import tqdm
 #tqdm.pandas()
 from info import UTC_to_NY
+from date_fixed_measures import oil_date
 
 if __name__ == '__main__':
     
@@ -15,11 +16,13 @@ if __name__ == '__main__':
     dtm_files.sort()
     
     df = pd.concat([pd.read_csv(file) for file in tqdm(dtm_files)])
-    df.loc[:,'TimeStamp'] = df.parallel_apply(lambda row: UTC_to_NY(row, to_str=False), axis=1)
-    df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
+    df.loc[:,'TimeStamp'] = df.parallel_apply(UTC_to_NY, axis=1)
+    df['TimeStamp_date'] = df['TimeStamp'].parallel_apply(lambda sample: oil_date(sample, store_date=True))
+    df['TimeStamp_date'] = pd.to_datetime(df['TimeStamp_date'])
     # groupby NYtime year and month
-    groups = df.groupby([df['TimeStamp'].dt.year, df['TimeStamp'].dt.month])
+    groups = df.groupby([df['TimeStamp_date'].dt.year, df['TimeStamp_date'].dt.month])
 
     for (year, month), group in tqdm(groups):
+        group.drop(columns=['TimeStamp_date'], inplace=True)
         filename = f"/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/NYtime_dtm_Clustering_C/{year}{month:02}_dtm.csv"
         group.to_csv(filename, index=False)
