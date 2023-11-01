094e3f2f4918147af5e72937c3835190cacb5453
Kaiwen Hou <60786982+Kaiwen-Hou-KHou@users.noreply.github.com>
2023-11-01 00:04:35 -0400

    Update run_Rolling_Topic_Models.sh
    change max of rolling index

--- OutOfSample/run_Rolling_Topic_Models.sh
+++ OutOfSample/run_Rolling_Topic_Models.sh
@@ -2,6 +2,6 @@
 
 chmod 700 Rolling_Topic_Models.py
 # sge_run --grid_mem=20G --grid_ncpus=1 --grid_submit=batch "./Rolling_Topic_Models.py --rolling_index=0 --GA_record_stats=True"
-for rolling_index in $(seq 0 266); do
+for rolling_index in $(seq 0 267); do
   sge_run --grid_mem=10G --grid_ncpus=1 --grid_submit=batch "./Rolling_Topic_Models.py --dtmPath=/shared/share_mamaysky-glasserman/energy_drivers/2023/DataProcessing/NYtime_dtm_numeric_441 --rolling_index=${rolling_index}"
 done
