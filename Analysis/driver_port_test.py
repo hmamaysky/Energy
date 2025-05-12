fa1044f67c95512dbfccefa23884344c96f3515a
hmamaysky <harry.mamaysky@gmail.com>
2025-05-12 00:38:37 -0400

    experiment with portfolio trading code

--- Analysis/driver_port_test.py
+++ Analysis/driver_port_test.py
@@ -1,4 +1,16 @@
 from Energy.Analysis import port_test as pt
 # %% read data
 pe = pt.PortEngine()
-pe.test_oosR2('text','FutRet')
+pe.check_true_rets('FutRet')
+pe.check_true_rets('xomRet')
+pe.check_true_rets('bpRet')
+pe.check_true_rets('rdsaRet')
+# %% double check some results from paper
+## a Figure 9 plot
+pe.select_oosR2('text','FutRet')
+
+## confirming the results in Table 6
+eo = pe.eval_oosR2(type='text', lag=3, tercile=2, weight=1, var='xomRet')
+# %% port sims
+ld,wts = pe.port_test('text','FutRet','FutRet',lag=4,tercile=2,thresh=0)
+ld,wts = pe.port_test('text','DSpot','FutRet',lag=4,tercile=2,thresh=0)
--- Analysis/port_test.py
+++ Analysis/port_test.py
@@ -1,35 +1,134 @@
 import numpy as np
 import pandas as pd
-__work_dir__ = '~/code/energy/outofsample/res_Forward_lasso/'
+import matplotlib.pyplot as plt
+__code_dir__ = '~/code/energy/'
 
 class PortEngine:
 
     def __init__(self):
 
         self.fname_map = {'text':'0-1','nontext':'1-0','both':'1-1'}
-        
-    def read_file(self,type,var):
+        self.select_end = '2009-11-30'
+        self.eval_start = '2010-01-01'
+
+        ## Shell PLC trades on Euronext Amsterdam; BP is the ADR return; FutRet is return on
+        ## USO ETF; XOM is from NYSE (the data are from Bloomberg). These are daily returns.
+        fname = __code_dir__+'Analysis/support/bbg_energy_with_divd_rets.csv'
+        print('Reading',fname)
+        self.rets = pd.read_csv(fname,parse_dates=['Dates'],index_col=0)
+
+        ## line up with fwd returns & double check timing
+        self.rets_fwd1d = self.rets.shift(-1)
+        assert self.rets_fwd1d.xomRet['2025-05-08'] == self.rets.xomRet['2025-05-09']
+
+    def read_lasso_file(self,type,var):
 
         assert type in self.fname_map.keys()
         
         ## read the data
-        fname = __work_dir__ + self.fname_map[type] + '_' + var + '.csv'
+        fname = __code_dir__ + 'outofsample/res_Forward_lasso/' + self.fname_map[type] + '_' + var + '.csv'
         print('Reading',fname)
         return pd.read_csv(fname,parse_dates=True,index_col=0)
 
     def __repr__(self):
 
-        repstr = f'fname_map: {self.fname_map}\n'
+        repstr = ''
         
-        for el in ['thed']:
-            if hasattr(self,el):
-                repstr = f'thed: data of size {getattr(self,el).shape}\n'
+        for el in ['fname_map','select_end','eval_start']:
+            repstr += f'{el}: {getattr(self,el)}\n'
 
+        for el in ['rets','rets_fwd1d']:
+            repstr += f'{el}: {getattr(self,el).shape}\n'
+            
         return repstr
-        
-    def test_oosR2(self,type,var):
 
-        thed = self.read_file(type,var)
+    
+    
+    ########## portfolio testing ##########
+
+    def port_test(self,type,var,retser,lag,tercile,thresh=2):
+        '''
+        type -- which signal to use: text, nontext, both
+        var -- which variable's lasso model to use, e.g. FutRet, DSpot, etc.
+        retser -- which market to trade, one of FutRet, xomRet, rdsaRet, and bpRet
+        lag -- which lag tercile to use
+        tercile -- value tercile at lag should have to trade
+        thresh -- threshold of forecast for taking a position
+        '''
+
+        ## sanity checks
+        assert retser in self.rets.columns
+        
+        ## get the lasso signals file
+        thed = self.read_lasso_file(type,var)
+        level = 100 if var == 'FutRet' else 0 ## levels across series
+        usesd = thed.true.std()
+        assert (level-0.2*usesd) <= thed.true.mean() <= (level+0.2*usesd) ## make sure have the correct level
+        
+        ## get weights and merge data
+        portd = pd.DataFrame(self.rets_fwd1d[retser][(self.rets_fwd1d.index >= self.eval_start) &
+                                                     (self.rets_fwd1d.index <= thed.index[-1])])
+        portd['weights'] = 0.
+
+        ## status
+        weight_AR = 0.
+        print(f'Portfolio simulation for {retser} using {type} model for {var} AR={weight_AR}')
+
+        ## set the weights from the selected signals
+        used = thed
+        ## used = thed[thed[f'lookback_tercile_lag_{lag:.1f}yr'] == tercile]
+        for ii,tt in enumerate(portd.index):
+
+            change_to_weight = 0
+
+            ## if date exists in the signal data, then set the weights from it
+            if tt in used.index:
+                if used.loc[tt,'true'] > level + thresh:
+                    portd.loc[tt,'weights'] = +1
+                elif used.loc[tt,'true'] < level - thresh:
+                    portd.loc[tt,'weights'] = -1
+
+            ## after first period, weight persists at AR=0.9 plus add update
+            if portd.loc[tt,'weights'] == 0 and ii > 0:
+                portd.loc[tt,'weights'] = portd.weights.iloc[ii-1]
+            #if ii == 0:
+            #    portd.loc[tt,'weights'] = change_to_weight
+            #else:
+            #    portd.loc[tt,'weights'] = weight_AR * portd.weights.iloc[ii-1] + change_to_weight
+
+        ## calculate each day's portfolio return
+        portd.weights = portd.weights.ffill()
+        port_rets = portd.weights * portd[retser]
+
+        ## get the underlying return series
+        jnt_rets = pd.DataFrame({retser:portd[retser],'port':port_rets})
+
+        fig, axs = plt.subplots(2,1)
+        (1+jnt_rets/100).cumprod().plot(title=f'Cumulative returns for {retser} using {var}\n' + \
+                                        f'signal {type} with lag={lag} and tercile={tercile}',ax=axs[0])
+        portd.weights.plot(ax=axs[1])
+        
+        return thed, portd.weights
+        
+    ########## replicate stuff in paper to make sure data/code are working ##########
+    
+    def get_forecast_and_oosR2(used,wt):
+        '''
+        Use blending weight to construct the blended signal and then calls the SSE-based
+        OOS R2.
+        '''
+        
+        blend = wt * used.pred + (1-wt) * used['mean']
+        return 100 - 100*((blend-used.true)**2).sum()/((used['mean']-used.true)**2).sum()
+
+    def select_oosR2(self,type,var):
+        '''
+        Replicate the in-sample model selection charts from Figure 8, the one with
+        rows corresponding to each dependent variable, i.e., FutRet, DSpot, DOilVol,
+        etc., and columns corresponding to the text, nontext, and both models.
+        '''
+        
+        thed = self.read_lasso_file(type,var)
         
         oosR2s = {}
         wts = np.arange(0,1.05,0.05)
@@ -40,14 +139,60 @@ class PortEngine:
             
             for wt in wts:
         
-                subd = thed[(thed.index <= '2009-11-30') &
+                subd = thed[(thed.index <= self.select_end) &
                             (thed['lookback_tercile_lag_4.0yr']==terc)]
-                blend = wt * subd.pred + (1-wt) * subd['mean']
-                oosR2[wt] = 100 - 100*((blend-subd.true)**2).sum()/((subd['mean']-subd.true)**2).sum()
+                oosR2[wt] = PortEngine.get_forecast_and_oosR2(subd,wt)
 
             oosR2s[terc] = oosR2
 
         oosR2s = pd.DataFrame(oosR2s)
-        ax = oosR2s.plot()
+        ax = oosR2s.plot(title=f'Forecasting model selection for {var} and {type}')
         ax.set_ylim(max(-5,oosR2s.min().min()),oosR2s.max().max()*1.02)
         ax.grid(color='lightgrey',alpha=0.5)
+
+    def eval_oosR2(self,type,lag,tercile,weight,var):
+        '''
+        Get the evaluation set out-of-sample R2s, as in Table 6. For each dependent
+        variable, e.g., FutRet, DSpot, etc., and a given lookback window, tercile level,
+        and weight, find the OOS R2s (which is completely out-of-sample). Only keep the
+        observation if the lagged tercile was in the right bucket.
+        '''
+
+        thed = self.read_lasso_file(type,var)
+
+        ## XXXHM: what is the timing of this date? Does the "true" return start on this date?
+        used = thed[(thed.index >= self.eval_start) &
+                    (thed[f'lookback_tercile_lag_{lag:.1f}yr'] == tercile)]
+        oosR2 = PortEngine.get_forecast_and_oosR2(used,weight)
+        print(f'Evaluation OOS R2 for {var} {type} = {oosR2:.4f}\n')
+        
+        return used
+
+    def check_true_rets(self,var):
+        '''
+        Sanity check on true returns.
+        '''
+
+        thet = self.read_lasso_file('text',var)
+        then = self.read_lasso_file('nontext',var)
+        theb = self.read_lasso_file('both',var)
+
+        ## make sure all three files are the same
+        assert (thet.true == then.true).all()
+        assert (thet.true == theb.true).all()
+
+        ## calculate fwd returns to double check
+        if var not in ['FutRet','xomRet', 'rdsaRet', 'bpRet']: return
+
+        ## get the fwd returns to double check
+        for tt in thet.index:
+            rets_fwd1d = self.rets_fwd1d[var].loc[(tt+pd.Timedelta(4,'w')):(tt+pd.Timedelta(12,'w'))]
+            thet.loc[tt,'Check'] = (1+rets_fwd1d/100).prod()*100
+
+        ## adjust the level (only FutRet shows gross returns)
+        if var != 'FutRet':
+            thet['Check'] -= 100
+            
+        thet[['true','Check']].plot(title=f'{var}')
+
+        
